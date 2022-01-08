a1d47efb81974f18d3776358690a3e113a480290
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2022-01-08 12:42:26 -0800

    updated area thresholds

--- hazard_data.py
+++ hazard_data.py
@@ -5,25 +5,28 @@ df = pd.read_csv('HazardousDrivingAreas.csv')
 
 
 def get_area(lat, lng, num=50):
-    # compute distance using Pythagoras
-    sorted_df = ((df.Latitude-lat)**2 + (df.Longitude-lng)**2).sort_values()[:num]
-    a = sorted_df.index.values.tolist()
+    distance_threshold = 0.05
+    reduced_df = df.loc[(df.Latitude-lat)**2 + (df.Longitude-lng)**2 <= distance_threshold**2]
+    print(reduced_df)
 
-    print(a)
-    reduced_df2 = df.loc(a) # todo: make this work
-    print(reduced_df2)
+    return reduced_df
+    # compute distance using Pythagoras
+    #sorted_df = ((df.Latitude-lat)**2 + (df.Longitude-lng)**2).sort_values()[:num]
+    #a = sorted_df.index.values.tolist()
+
+    #print(a)
+    #reduced_df2 = df.loc(a) # todo: make this work
+    #print(reduced_df2)
 
 
 def areas_to_avoid():
-    # todo have a threshold value for places to avoid
+    # todo figure out a threshold value for places to avoid
     severity_threshold = 0.02
     incidents_threshold = 5
-    reduced_df = df.loc[df['SeverityScore'] >= severity_threshold ]
-    print(reduced_df)
+    reduced_df = df.loc[df['SeverityScore'] >= severity_threshold]
+    #print(reduced_df)
+    return reduced_df
 
-    # for index, row in df.iterrows():
-    #     if(row.SeverityScore>0.01):
-    #         print(row)
     pass
 
 # misc testing
@@ -31,5 +34,5 @@ def areas_to_avoid():
 #print(df)
 #print(df.Latitude)
 
-#get_area(43.7,-79.5)
-areas_to_avoid()
+get_area(43.7,-79.5)
+#areas_to_avoid()
