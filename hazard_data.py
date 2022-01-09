1af0962dc3fbdf7b44badee26f531eeb452d85f8
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2022-01-08 19:50:02 -0800

    more data gathering

--- hazard_data.py
+++ hazard_data.py
@@ -3,9 +3,7 @@ import numpy as np
 
 df = pd.read_csv('HazardousDrivingAreas.csv')
 
-
-def get_area(lat, lng, num=50):
-    distance_threshold = 0.05
+def get_area(lat, lng, distance_threshold = 0.05):
     nearby_df = df.loc[(df.Latitude-lat)**2 + (df.Longitude-lng)**2 <= distance_threshold**2]
 
     #print(nearby_df)
@@ -16,9 +14,11 @@ def get_area(lat, lng, num=50):
 def areas_to_avoid():
     # todo figure out a good threshold value for places to avoid
     severity_threshold = 0.02
-    incidents_threshold = 5
+    incidents_threshold = 10
     reduced_df = df.loc[df['SeverityScore'] >= severity_threshold]
-    #print(reduced_df)
+    reduced_df = reduced_df.loc[reduced_df['IncidentsTotal'] >= incidents_threshold]
+    reduced_df.to_csv("hazards_above_threshold.csv")
+    print(reduced_df)
     return reduced_df
 
     pass
@@ -29,4 +29,4 @@ def areas_to_avoid():
 #print(df.Latitude)
 
 #get_area(43.7,-79.5)
-#areas_to_avoid()
+areas_to_avoid()
